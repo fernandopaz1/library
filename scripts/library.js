13600d28dbd4f6b747a1c778bac157891c9649ff
Fernando Paz <ferpaz9292@gmail.com>
2020-09-03 22:34:48 -0300

    definition of info() with prototype inheritance

--- scripts/library.js
+++ scripts/library.js
@@ -1,12 +1,17 @@
-function Book(title,author,pages,readed){
-    this.title=title;
-    this.author=author;
-    this.pages=pages;
-    this.readed=readed;
-    this.info=()=>{
-        let s=this.readed ? "readed" : "not readed yet";
-        return `${this.title} by ${this.author}, ${pages} pages, ${s}`;
-    }
+function Book(title, author, pages, readed) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.readed = readed;
+    // this.info=()=>{
+    //     let s=this.readed ? "readed" : "not readed yet";
+    //     return `${this.title} by ${this.author}, ${pages} pages, ${s}`;
+    // }
 }
 
-let book1=new Book("Hola","mundo",11,true);
+Book.prototype.info = function () {
+    let s = this.readed ? "readed" : "not readed yet";
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${s}`;
+}
+
+let book1 = new Book("Hola", "mundo", 11, true);
