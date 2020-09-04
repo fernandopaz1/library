31a7109f5f466ddc80795371d02e2f929862b9ef
Fernando Paz <ferpaz9292@gmail.com>
2020-09-03 21:46:20 -0300

    book constructor added, and method info()

---
+++ scripts/library.js
@@ -0,0 +1,12 @@
+function Book(title,author,pages,readed){
+    this.title=title;
+    this.author=author;
+    this.pages=pages;
+    this.readed=readed;
+    this.info=()=>{
+        let s=this.readed ? "readed" : "not readed yet";
+        return `${this.title} by ${this.author}, ${pages} pages, ${s}`;
+    }
+}
+
+let book1=new Book("Hola","mundo",11,true);
