4addcbb884fb78908fd037e7e12e8a467d7273a2
Fernando Paz <ferpaz9292@gmail.com>
2020-09-05 23:20:48 -0300

    adding css to booknfo show in page

--- scripts/library.js
+++ scripts/library.js
@@ -15,6 +15,7 @@ Book.prototype.info = function () {
 }
 
 let book1 = new Book("Hola", "mundo", 11, true);
+let book2 = new Book("Harry Potter", 500,false);
 
 
 btnNewBook.addEventListener("click", addBookToLibrary)
@@ -28,8 +29,9 @@ function addBookToLibrary() {
 }
 
 showBook(book1);
+showBook(book2);
 
-function showBook(book) {  
+function showBook(book) {
     let title = `<h1  class=${"title"}>${book.title}</h1>`;
     let author = `<h3  class=${"author"}>${book.author}</h3>`;
     let pages = `<h4  class=${"pages"}>${book.pages}</h4>`;
@@ -38,23 +40,28 @@ function showBook(book) {
 
     let btnDelete = `<button id=${`del${book.title}`} class=${"btnDelete"}>Delete</button>`
     let btnReaded = `<button id=${`read${book.title}`} class=${"btnReaded"}>Readed</button>`
-    let btnContainer = `<div id=${`${book.title}`}>${btnDelete} ${btnReaded}</div>` 
+    let btnContainer = `<div id=${`${book.title}`}>${btnDelete} ${btnReaded}</div>`
 
-    let content = `<div id=${book.title}>${title} ${author} ${pages} ${readed} ${btnContainer}</div>`
+    let content = `<div id=${book.title} class="bookInfo">${title} ${author} ${pages} ${readed} ${btnContainer}</div>`
 
     bookContainer.innerHTML += content
-    
+
     addButton();
 }
 
 
-function addButton(){
-    
-    document.querySelectorAll(".btnDelete").forEach((button)=>{
-        button.addEventListener("click",(e)=>{
-            console.log(`this is the event ${e}`)
+function addButton() {
+    document.querySelectorAll(".btnDelete").forEach((button) => {
+        button.addEventListener("click", (e) => {
             let elem = e.target;
-            let bookCont=elem.parentNode.parentNode;
+            let bookCont = elem.parentNode.parentNode;
+            bookCont.parentNode.removeChild(bookCont);
+        })
+    })
+    document.querySelectorAll(".btnReaded").forEach((button) => {
+        button.addEventListener("click", (e) => {
+            let elem = e.target;
+            let bookCont = elem.parentNode.parentNode;
             bookCont.parentNode.removeChild(bookCont);
         })
     })
